6f277bc41433f9c0dc4d170b33c39b4a4a2cfd9a
Xianith <xianith@gmail.com>
2016-09-19 22:13:45 +0000

    initial Commin -- AWS Hydra

---
+++ index.php
@@ -0,0 +1,89 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>DoC</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bs/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="ie-emulation-modes-warning.js"></script>
+    
+  <!-- Contains custom Css and Jumbotron CSS -->
+    <link href="css/doc.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="/web/20160314200512/https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="/web/20160314200512/https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+
+
+<header class="clearfix">
+    <h3>Drinkin Outa Cups</h3>
+  </header>
+
+    <div class="container">
+
+      <div class="jumbotron">
+        <h1>Welcome</h1>
+        <p class="lead">We play games n' stuff</p>
+        <p><a class="btn btn-lg btn-success" href="https://discord.gg/0aVDKqSEos1SAKGd" role="button">Connect</a></p>
+      </div>
+
+      <div class="row marketing">
+        <div class="col-lg-6">
+		    <script type="text/javascript" src="java/discord.min.js"></script>
+		    <script type="text/javascript">
+		      discordWidget.init({
+		        serverId: '105404204360667136',
+		        title: '',
+		        join: false,
+		        alphabetical: false,
+		        theme: 'none',
+		        hideChannels: ['AFK', 'General', 'Room'],
+		        showAllUsers: true,
+		        allUsersDefaultState: true
+		      });
+		      discordWidget.render();
+		    </script>
+
+	      	<h4>Online Members</h4>
+	      	<div class="discord-widget"></div>
+	      		<p></p>
+  			<!-- <iframe src="/web/20160314200512/https://discordapp.com/widget?id=105404204360667136&theme=light" width="300" height="350" allowtransparency="true" frameborder="0"></iframe> -->
+        </div>
+
+        <div class="col-lg-6">
+          <h4>Communities</h4>
+          <a class="nav-images" href="http://steamcommunity.com/groups/drinkinoutacups"><img src="img/steam.png" height="35px" padding-right="5px"></img></a>
+          <a class="nav-images" href="https://discord.gg/0aVDKqSEos1SAKGd"><img src="img/discord.png" height="35px"></img></a>
+      </div>
+
+   <!--    <footer class="footer">
+        <p>&copy; 2015 Unloaded, Inc.</p>
+      </footer>
+ -->
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="bs/java/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
