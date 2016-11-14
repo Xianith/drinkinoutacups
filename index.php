9f22e687f366dc8a9f8713593d4e79e6f9548af4
Xianith <xianith@gmail.com>
2016-11-14 22:28:28 +0000

    Filter users, hide top game

--- index.php
+++ index.php
@@ -4,25 +4,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
+    <meta name="description" content="Vidya game group">
+    <meta name="author" content="Ben">
+    <link rel="icon" href="img/favicon.ico">
 
-    <title>DoC</title>
+    <title>D O C</title>
 
     <!-- Bootstrap core CSS -->
     <link href="bs/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <!-- <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet"> -->
 
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="ie-emulation-modes-warning.js"></script>
+    <!-- <script src="ie-emulation-modes-warning.js"></script> -->
     
   <!-- Contains custom Css and Jumbotron CSS -->
     <link href="css/doc.css" rel="stylesheet">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script type="text/javascript" src="java/game.js"></script>
+
+    <!-- Discord Java -->
+    <script type="text/javascript" src="java/discord.min.js"></script>
+    <script type="text/javascript">
+      discordWidget.init({
+        serverId: '105404204360667136',
+        title: '',
+        join: false,
+        alphabetical: false,
+        theme: 'none',
+        hideChannels: ['AFK', 'General', 'Room'],
+        showAllUsers: true,
+        allUsersDefaultState: true
+      });
+      discordWidget.render();
+    </script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -33,57 +51,56 @@
 
   <body>
 
-
-
-<header class="clearfix">
+  <header class="clearfix">
     <h3>Drinkin Outa Cups</h3>
   </header>
 
-    <div class="container">
+  <div class="container">
 
-      <div class="jumbotron">
-        <h1>Welcome</h1>
-        <p class="lead">We play games n' stuff</p>
-        <p><a class="btn btn-lg btn-success" href="https://discord.gg/0aVDKqSEos1SAKGd" role="button">Connect</a></p>
+    <div class="jumbotron">
+      <h1>Hey there!</h1>
+      <p class="lead">We have fun playing vidya games...</p>
+      <p><a class="btn btn-lg btn-success" href="https://discord.gg/0aVDKqSEos1SAKGd" role="button">Join us</a></p>
+    </div>
+
+    <div class="row marketing">
+      <div class="col-lg-6">
+
+      	<h4>Online Members</h4>
+      	<div class="discord-widget"></div>
       </div>
 
-      <div class="row marketing">
-        <div class="col-lg-6">
-		    <script type="text/javascript" src="java/discord.min.js"></script>
-		    <script type="text/javascript">
-		      discordWidget.init({
-		        serverId: '105404204360667136',
-		        title: '',
-		        join: false,
-		        alphabetical: false,
-		        theme: 'none',
-		        hideChannels: ['AFK', 'General', 'Room'],
-		        showAllUsers: true,
-		        allUsersDefaultState: true
-		      });
-		      discordWidget.render();
-		    </script>
-
-	      	<h4>Online Members</h4>
-	      	<div class="discord-widget"></div>
-	      		<p></p>
-  			<!-- <iframe src="/web/20160314200512/https://discordapp.com/widget?id=105404204360667136&theme=light" width="300" height="350" allowtransparency="true" frameborder="0"></iframe> -->
-        </div>
-
-        <div class="col-lg-6">
-          <h4>Communities</h4>
-          <a class="nav-images" href="http://steamcommunity.com/groups/drinkinoutacups"><img src="img/steam.png" height="35px" padding-right="5px"></img></a>
-          <a class="nav-images" href="https://discord.gg/0aVDKqSEos1SAKGd"><img src="img/discord.png" height="35px"></img></a>
+      <div class="col-lg-6">
+        <h4>Communities</h4>
+        <a class="nav-images" href="http://steamcommunity.com/groups/drinkinoutacups"><img src="img/steam.png" height="35px" padding-right="5px"></img></a>
+        <a class="nav-images" href="https://discord.gg/0aVDKqSEos1SAKGd"><img src="img/discord.png" height="35px"></img></a>
       </div>
 
-   <!--    <footer class="footer">
-        <p>&copy; 2015 Unloaded, Inc.</p>
-      </footer>
- -->
-    </div> <!-- /container -->
+<!--       <div class="col-lg-12 top-games">
+        <h4>Top Game</h4>
+        
+        <ol class="game-list">
 
+        </ol>
 
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="bs/java/ie10-viewport-bug-workaround.js"></script>
+          <div class="game">
+            <img src="http://orig08.deviantart.net/65e3/f/2014/207/e/2/official_wow_icon_by_benashvili-d7sd1ab.png" height="85px" padding-right="5px"></img>
+            <div class="game-stats">
+              <h2>World of Warcraft</h2>
+            </div>
+          </div> -->
+
+          <!-- <div class="game">
+            <a class="nav-images" href="http://steamcommunity.com/groups/drinkinoutacups"><img src="img/steam.png" height="85px" padding-right="5px"></img></a>
+            <div class="game-stats">
+              <h2>World of Warcraft</h2>
+              <b>Total Time:</b>
+            </div>
+          </div> -->
+      <!-- </div> -->
+
+    </div>
+
+  </div>
   </body>
 </html>
