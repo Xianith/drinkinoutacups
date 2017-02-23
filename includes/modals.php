535342352acbff59552e31095bcc0943da884bdf
Xianith <xianith@gmail.com>
2017-02-23 19:52:07 +0000

    Added new modal page.

---
+++ includes/modals.php
@@ -0,0 +1,156 @@
+<div id="alphaLoot" class="modal">
+  <span class="close cursor">&times;</span>
+  <div class="modal-content">
+
+    <div class="alphaLoot">
+      <div class="slideTitle">alphaLoot - Config snippet</div>
+
+      <div class="codeBlock">
+      <pre><code>
+      {
+        "ItemListBarrels": {
+          "propanetank": 2,
+          "riflebody": 2,
+          "roadsigns": 5,
+          "rope": 5,
+          "semibody": 2,
+          "sewingkit": 2,
+          "sheetmetal": 5
+        },
+        "ItemListCrates": {
+          "riflebody": 5,
+          "roadsigns": 5,
+          "rope": 5,
+          "semibody": 5,
+          "sewingkit": 5,
+          "sheetmetal": 5,
+          "smallwaterbottle": 1,
+          "smgbody": 5,
+          "metalspring": 5
+        }
+      }
+      </code></pre>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div id="gatherManager" class="modal">
+  <span class="close cursor">&times;</span>
+  <div class="modal-content">
+
+    <div class="gatherManager">
+      <div class="slideTitle">GatherManager - Config snippet</div>
+
+      <div class="codeBlock">
+      <pre><code>
+      "Options": {
+        "GatherDispenserModifiers": {},
+        "GatherResourceModifiers": {
+          "*": 10.0,
+          "Cloth": 10.0,
+          "Stones": 10.0,
+          "Sulfur Ore": 10.0,
+          "Wood": 10.0
+        },
+        "MiningQuarryResourceTickRate": 5.0,
+        "PickupResourceModifiers": {
+          "*": 10.0
+        },
+        "QuarryResourceModifiers": {
+          "Stones": 25.0,
+          "Sulfur Ore": 25.0,
+          "Metal Ore": 15.0
+        },
+        "SurveyResourceModifiers": {}
+      </code></pre>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div id="quickSmelt" class="modal">
+  <span class="close cursor">&times;</span>
+  <div class="modal-content">
+
+    <div class="quickSmelt">
+      <div class="slideTitle">QuickSmelt - Config snippet</div>
+
+      <div class="codeBlock">
+      <pre><code>
+      {
+        "ByproductModifier": 25.0,
+        "ByproductPercent": 50,
+        "CookedModifier": 10.0,
+        "CookedPercent": 100,
+        "FuelUsageModifier": 1,
+        "OvercookMeat": false,
+        "UsePermissions": false
+      }
+      </code></pre>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div id="stackSize" class="modal">
+  <span class="close cursor">&times;</span>
+  <div class="modal-content">
+
+    <div class="stackSize">
+      <div class="slideTitle">StackSizeController - Config snippet</div>
+
+      <div class="codeBlock">
+      <pre><code>
+      ...
+      "Mushroom": 50,
+      "Muzzle Boost": 1,
+      "Muzzle Brake": 1,
+      "Note": 250000,
+      "One Sided Town Sign Post": 250000,
+      "Pants": 1,
+      "Paper": 250000,
+      "Paper Map": 250000,
+      "Pick Axe": 1,
+      "Pistol Bullet": 250000,
+      "Pookie Bear": 250000,
+      ...
+
+      ...
+      "Sticks": 250000, //Removed via other plugin
+      "Stone Barricade": 10,
+      "Stone Hatchet": 1,
+      "Stone Pick Axe": 1,
+      "Stone Spear": 1,
+      "Stones": 250000,
+      "Sulfur": 250000,
+      "Sulfur Ore": 250000,
+      ...
+      </code></pre>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="changeLog" class="modal">
+  <span class="close cursor">&times;</span>
+  <div class="modal-content">
+
+    <div class="stackSize">
+      <div class="slideTitle">Change Log</div>
+
+      <div class="clog">
+        <i>2/22</i>
+          <p class="minus">Modified gold shop rewards (removed several T2 for balance)</p>
+          <p class="plus">Added Poll plugin</p>
+          <p class="plus">Modified VIP chat color (was blue, now gold)</p>
+          <p class="plus">Granted autograde.3 and autograde.4 to vip group (Metal / HQM)</p>
+          <p class="plus">Added Bounty plugin (With popup depency plugin)</p>
+          <p class="minus">Re-enabled stability and cleared a lot of un-cupboarded entities</p>
+      </div>
+    </div>
+  </div>
+</div>
